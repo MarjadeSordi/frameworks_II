022c02979398c25fe42f679ddfb11c8d553b1690
Marja de Sordi <marjadesordi@gmail.com>
2022-08-18 20:24:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+var router = express.Router();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/', function (req, res) {
+    res.send('GET request to the homepage');
+});
+
+
+app.get('/user/:id', function (req, res) {
+    res.send('OK')
+})
+
+app.post('/', function (req, res) {
+    res.send('POST request to homepage')
+})
+
+app.put('/', function (req, res) {
+    res.send('PUT request to homepage')
+})
+
+app.delete('/', function (req, res) {
+    res.send('DELETE request to homepage')
+})
+
+
